c46e48d5bb82625df50bb5d71b30b4b0b001486d
Purushotam-Cogent <purushotam.sinha@cogentinfo.com>
2021-04-21 10:09:28 -0400

    setter/getter

--- src/main/java/com/example/demo/DataController.java
+++ src/main/java/com/example/demo/DataController.java
@@ -54,6 +54,33 @@ class Data implements Serializable
 		this.id = id;
 		this.name = name;
 	}
+	
+	
+	
+	public int getId() {
+		return id;
+	}
+
+
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
 	@Override
 	public String toString() {
 		return "Data [id=" + id + ", name=" + name + "]";
